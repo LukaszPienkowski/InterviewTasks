d7f9acd7bd5b80253931536e7b456fa9ff4cf5f1
LukaszPienkowski <78901940+LukaszPienkowski@users.noreply.github.com>
2021-02-23 21:39:29 +0100

    Update and rename RomanNumbs.py to RomanNumbs_PayPal.py

--- RomanNumbs_PayPal.py
+++ RomanNumbs_PayPal.py
@@ -1,3 +1,5 @@
+## Replacing num 1 - 1000 with its Roman equivalent
+
 def ChangeArabicIntoRoman(ArabicNum):
     romantransforms = {'1': "I", '2': "II", '3': "III", '4': "IV", '5': "V", '6': "VI", '7': "VII", '8': "VIII", '9': "IX"} 
     romantransformd = {'1': "X", '2': "XX", '3': "XXX", '4': "XL", '5': "L", '6': "LX", '7': "LXX", '8': "LXXX", '9': "XC"}
